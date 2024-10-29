904de3728be7893ae1698b4e88bbc0bbac7a4221
Rick Maas <rmaas939@gmail.com>
2024-10-29 16:03:07 +0100

    Starting over with back-end project, this time we have a diffrent approach with the login system

--- routes/web.php
+++ routes/web.php
@@ -11,44 +11,32 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/logout', function () {
-    return view('welcome');
-})->middleware(['auth', 'verified'])->name('logout');
+// // Route::get('/', function () {
+// //     return view('welcome');
+// // })->middleware(['auth
 
-// Route::get('/', function () {
-//     return view('welcome');
-// })->middleware(['auth', 'verified'])->name('/');
+// Route::get('/addbooks', function(){
+//     return view('addbooks');
+// });
 
-Route::post('/logout', function (Request $request) {
-    Auth::logout();
-    $request->session()->invalidate();
-    $request->session()->regenerateToken();
+// Route::get('/addbooks', [BookController::class, 'index'])->name('addbooks');
+// Route::post('/addbooks/submit', [BookController::class, 'submitbook'])->name('addbooks/submit');
+// Route::get('/', [BookController::class, 'store'])->name('welcome');
 
-    return redirect('/');
-})->name('logout');
-
-Route::get('/addbooks', function(){
-    return view('addbooks');
-});
-
-Route::get('/addbooks', [BookController::class, 'index'])->name('addbooks');
-Route::post('/addbooks/submit', [BookController::class, 'submitbook'])->name('addbooks/submit');
-Route::get('/', [BookController::class, 'store'])->name('welcome');
-
-// Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
-// Route::post('login', [LoginController::class, 'login']);
-// Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+// // Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+// // Route::post('login', [LoginController::class, 'login']);
+// // Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
 
-Route::get('/bookdetails', function () {
-    return view('bookdetails');
-})->name('bookdetails');
+// Route::get('/bookdetails', function () {
+//     return view('bookdetails');
+// })->name('bookdetails');
 
-Route::get('/bookdetails/{id}', [BookController::class, 'get_id'])->name('bookdetails.id');
+// Route::get('/bookdetails/{id}', [BookController::class, 'get_id'])->name('bookdetails.id');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
